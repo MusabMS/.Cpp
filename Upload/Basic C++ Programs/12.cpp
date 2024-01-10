8ecd6dfec612e2915dbe7fbf6b23557c067e4683
Musab Shaikh <154394785+MusabMS@users.noreply.github.com>
2024-01-10 12:05:55 +0530

    Update 12.cpp

--- Upload/Basic C++ Programs/12.cpp
+++ Upload/Basic C++ Programs/12.cpp
@@ -1,4 +1,4 @@
-// Compound Interest
+// Compound Interest (with FUNCTION)
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -11,3 +11,20 @@ int main() {
 
     return 0;
 }
+
+
+// Compound Interest (without FUNCTION)
+#include <iostream>
+using namespace std;
+
+int main() {
+    double p = 1000, r = 10, t = 2, CI = 1;
+    r = r/100, r += 1;
+    for(int i=1; i<=t; i++){
+        CI *= r;}
+    CI = ((p * CI) - p);
+    cout << CI;
+
+    return 0;
+}
+
